05411c6578ef54654dc51e94a0e9dfac5d391863
siyuan9826@gmail.com <coder@Coders-MacBook-Pro.local>
2020-02-22 08:49:54 +0800

    siyuan-gao

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,24 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+const header2 = document.querySelector('header')
 
-function Header() {}
+function Header() {
+  const headerDiv = document.createElement('div')
+  headerDiv.classList.add('header')
+
+  const headerDate = document.createElement('span')
+  headerDate.classList.add('date')
+  headerDiv.appenChild('headerDate')
+
+  const headerTitle = document.createElement('h1')
+  headerTitle.textContent = "Lambda Times"
+  headerDiv.appendChild('h1')
+
+  const headerTemp = document.createElement('span')
+  headerTemp.classList.add('temp')
+  headerDiv.appendChild('headerTemp')
+  headerTemp.textContent = "98°"
+
+  return headerDiv;
+}
